5a4e8feabe7a5a7e87de32e8cd3f130a08843540
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-02 18:26:08 -0300

    Tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,26 @@
+var idade = 16
+var simbolo = Symbol();
+console.log(typeof simbolo)
+
+var nome = 'Eduardo'
+var sobrenome = "Santos"
+var nomeCompleto = nome + ' '+ sobrenome
+
+console.log(nomeCompleto);
+
+var gols = 100
+var frase = 'Neymar fez' + gols + 'gols'
+
+console.log(typeof frase)
+
+var ano = 2018;
+var mes = 8;
+console.log(ano + mes)
+
+
+var teste = 'melhor'
+var frase1 = "Esse é o " + teste + " jogo"
+console.log (frase1)
+
+var gols = 1000
+var frase2 = `Romário fez ${gols * 2} gols`
