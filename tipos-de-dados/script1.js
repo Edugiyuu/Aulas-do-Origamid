0e550bf5eca36d0997c0dbca61990135e8f49f87
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-03 17:33:44 -0300

    Update script1.js

---
+++ tipos-de-dados/script1.js
@@ -0,0 +1,18 @@
+// Declare uma variável contendo uma string
+var nome = 'Eduardo';
+// Declare uma variável contendo um número dentro de uma string
+var ano = '2018'
+// Declare uma variável com a sua idade
+var idade = 16;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var sobrenome = 'Paz'
+var nomeCompleto = `${nome} ${sobrenome}`
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "it's time";
+
+// Verifique o tipo da veariável que contém o seu nome
+var verificarTiponome = typeof nome;
+
+console.log(verificarTiponome)
