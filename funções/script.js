067f57acb1389c3bbc20f40ea8827dda9b413ea1
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-16 22:54:36 -0300

    Update script.js

--- funções/script.js
+++ funções/script.js
@@ -58,5 +58,22 @@ console.log (terceiraIdade(60))
 
 function faltaVisitar(paisesVisitados){
     var totalPaises = 193;
-    return `Faltam visitar`
+    return `Faltam visitar ${totalPaises - paisesVisitados} países`
 }
+console.log(totalPaises)
+
+var profissao = 'Designer';
+
+function dados() {
+  var nome = 'André';
+  var idade = 28;
+  function outrosDados() {
+    var endereco = 'Rio de Janeiro';
+    var idade = 29;
+    return `${nome}, ${idade}, ${endereco}, ${profissao}`;
+  }
+  return outrosDados();
+}
+
+console.log(dados()); // Retorna 'André, 29, Rio de Janeiro, Designer'
+outrosDados(); // retorna um erro
