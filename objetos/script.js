18992619129e2fb8b0f199aa1ed3e52ce56626ba
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-21 22:52:14 -0300

    Objetos

---
+++ objetos/script.js
@@ -0,0 +1,15 @@
+var pessoa = {
+    nome:'André',
+    idade:28,
+}
+
+console.log (pessoa);
+
+var quadrado = {
+    lados:4,
+    area: function(lado){
+        return lado * lado;
+    }
+}
+
+console.log (quadrado.area(5))
