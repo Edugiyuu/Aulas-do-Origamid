3367e40366d16d272db508140a1e6afd9f86dd96
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2022-09-24 23:43:24 -0300

    Update script.js

--- objetos/script.js
+++ objetos/script.js
@@ -9,7 +9,13 @@ var quadrado = {
     lados:4,
     area: function(lado){
         return lado * lado;
+    },
+    perimetro (lado) {
+        return this.lados * lado;
+    },
+    cinco(){
+        return 5;
     }
 }
-
-console.log (quadrado.area(5))
+console.log(quadrado.area(5));
+console.log (quadrado.area(5));
