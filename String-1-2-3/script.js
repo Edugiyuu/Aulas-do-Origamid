b48a3805da8e35bd36ec361dd10c65dc9b0bfce3
Edugiyuu <102703153+Edugiyuu@users.noreply.github.com>
2023-01-26 16:53:44 -0300

    String 1

---
+++ String-1-2-3/script.js
@@ -0,0 +1,9 @@
+const comida = 'Pizza';
+const agua = new String('agua')
+
+console.log(agua.length);
+
+const frase = 'A melhor comida'
+
+console.log(frase[frase.length - 1]);
+console.log(frase.charAt[frase.length - 1]);
